c98b68bc88a45249d83bff699c7d573e5665c8cb
Linnaar <a.p.goloborodko@gmail.com>
2012-09-05 22:26:11 +0600

    rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hz::Application.config.secret_token = 'c4e9487c9c7009dbf2664564f2c514c03dab25067eec2a8ad1113e5edfd31afae63c2fe11ff5bdf49d25a3be104f8b02494054e914517aafe2874c4b25c480a3'
